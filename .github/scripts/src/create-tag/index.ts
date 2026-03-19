03c1e8d0ddf37f508f8662e7aeea321d6a621f8d
Tahmid Ahmed <tahmid@tahmid.io>
2026-03-19 01:56:58 -0400

    Update @tahminator/pipeline ver + use utils fn to update package.jsons with new version tag

--- .github/scripts/src/create-tag/index.ts
+++ .github/scripts/src/create-tag/index.ts
@@ -1,5 +1,4 @@
 import { GitHubClient, Utils } from "@tahminator/pipeline";
-import { $ } from "bun";
 
 export async function main() {
   const ciEnv = await Utils.getEnvVariables(["ci"]);
@@ -9,24 +8,7 @@ export async function main() {
 
   await ghClient.createTag({
     onPreTagCreate: async (tag) => {
-      const files = (
-        await $`find . -name "package.json" -not -path "*/node_modules/*"`.text()
-      )
-        .trim()
-        .split("\n");
-
-      for (const fileLocation of files) {
-        const file = Bun.file(fileLocation);
-
-        const pkg = await file.json();
-        pkg.version = tag;
-
-        await Bun.write(fileLocation, JSON.stringify(pkg, null, 2) + "\n");
-
-        console.log(
-          `Successfully updated version in ${fileLocation} to ${tag}`,
-        );
-      }
+      await Utils.updateAllPackageJsonsWithVersion(tag);
     },
   });
 }
